ee9510aae376a1776fa2cda4f0a06dabe11a94c1
Pratham Singh <prathms007@gmail.com>
2020-01-16 17:41:19 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const firebase = require("firebase");
+require("firebase/firestore");
+
+firebase.initializeApp({
+    apiKey: "AIzaSyAU-IpgxblGitBO8KO2XKZG38n5gGzSAcM",
+    authDomain: "chatter-586c6.firebaseapp.com",
+    databaseURL: "https://chatter-586c6.firebaseio.com",
+    projectId: "chatter-586c6",
+    storageBucket: "chatter-586c6.appspot.com",
+    messagingSenderId: "958899200756",
+    appId: "1:958899200756:web:17111cde0b29f7bbf9b4a1",
+    measurementId: "G-GYBDB7S3EE"
+ });
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
