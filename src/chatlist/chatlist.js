16608cef8294e79a7e962c1de413d2f6208720a3
Pratham Singh <prathms007@gmail.com>
2020-01-17 15:29:04 +0530

    added chat list to dashboard with styles

---
+++ src/chatlist/chatlist.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+import styles from './styles';
+import Divider from '@material-ui/core/Divider';
+import Button from '@material-ui/core/Button';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import NotificationImportant from '@material-ui/icons/NotificationImportant';
+
+
+function ChatList(props) {
+    const { classes } = props;
+
+    const newChat = () => {
+
+    }
+    
+    const selectChat = (index) => {
+        console.log('select chat', index);
+    }
+
+    if (props.chats.length > 0) {
+        return (
+            <main className={classes.root}>
+                <Button variant='contained' fullWidth color='primary'
+                    className={classes.newChatBtn} onClick={newChat}>New Message</Button>
+                <List>
+                    {
+                        props.chats.map((_chat, _index) => {
+                            return (
+                                <div key={_index}>
+                                    <ListItem onClick={() => {selectChat(_index);}} 
+                                        className={classes.listItem}
+                                        selected={props.selectedChatIndex === _index}
+                                        alignItems='flex-start'>
+                                            <ListItemAvatar>
+                                                <Avatar alt='Remy Sharp'>{_chat.users.filter(_user => { return _user !== props.userEmail; })[0].split('')[0]}</Avatar> 
+                                            </ListItemAvatar>
+                                            <ListItemText primary={_chat.users.filter(_user => { return _user !== props.userEmail; })[0]}
+                                                secondary={
+                                                    <React.Fragment>
+                                                        <Typography component='span' color='textPrimary'>
+                                                            {
+                                                                _chat.messages[_chat.messages.length - 1].message.substring(0, 30)
+                                                            }
+                                                        </Typography>
+                                                    </React.Fragment>
+                                                } >
+                                            </ListItemText>
+                                    </ListItem>
+                                    <Divider></Divider>
+                                </div>
+                            );
+                        })
+
+                    }
+                </List>
+            </main>
+        );
+    } else {
+        return (
+            <main className={classes.root}>
+                <Button variant='contained' fullWidth onClick={newChat}
+                    className={classes.newChatBtn}>New Message</Button>
+               <List></List> 
+            </main>
+        );
+    }
+}
+
+export default withStyles(styles)(ChatList);
--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -1,9 +1,59 @@
 import React, {useEffect, useState} from 'react';
+import ChatList from '../chatlist/chatlist';
+import { Button , withStyles } from '@material-ui/core';
+import styles from './styles';
+const firebase = require('firebase');
 
-function DashboardComponent() {
+function DashboardComponent(props) {
+    const [selectedChat, setSelectedChat] = useState(null);
+    const [newChatFormVisible, setNewChatFormVisible] = useState(false);
+    const [email, setEmail] = useState(null);
+    const [chats, setChats] = useState([]);
+
+    const {classes} = props;
+
+    useEffect( () => {
+        firebase.auth().onAuthStateChanged(async _usr => {
+            if (!_usr) {
+                props.history.push('/login');
+            } else {
+                await firebase
+                    .firestore()
+                    .collection('chats')
+                    .where('users', 'array-contains', _usr.email)
+                    .onSnapshot(async res => {
+                        const chats = res.docs.map(_doc => _doc.data());
+                        setEmail(_usr.email);
+                        setChats(chats);
+                        console.log(chats);                       
+                    });
+
+            }
+        })
+    }, []);
+
+    const selectChat = (chatIndex) => {
+        console.log('Select a chat.', chatIndex);
+    }
+    const newChatBtnClicked = () => {
+       setNewChatFormVisible(true);
+       setSelectedChat(null); 
+    }
+    const signOut = () => {
+        firebase.auth().signOut();
+    }
+
     return (
-		<div>Hello World</div>
+		<div>
+            <ChatList history={props.history} 
+            newChatBtnFn={newChatBtnClicked}
+            selectChatFn={selectChat}
+            userEmail={email}
+            selectedChatIndex={selectedChat}
+            chats={chats} />
+            <Button className={classes.signOutBtn} onClick={signOut}>Sign Out</Button>
+        </div>
 	);
 }
 
-export default DashboardComponent;
+export default withStyles(styles)(DashboardComponent);
